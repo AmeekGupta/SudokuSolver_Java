6e3195c7dd7261920f55c86ce3ef78a763ddcc41
Ameek <gupta.ameek94.ga@gmail.com>
2022-02-01 11:49:39 -0500

    initial commit

---
+++ src/SudokuSolver.java
@@ -0,0 +1,108 @@
+/*
+    SUDOKU SOLVER
+*/
+public class SudokuSolver {
+    // grid size
+    private static final int GRID_SIZE = 9;
+    // main method
+    public static void main(String[] args) {
+        // Creating sudoku board with 2d array
+        int[][] board = {
+                {7, 0, 2, 0, 5, 0, 6, 0, 0},
+                {0, 0, 0, 0, 0, 3, 0, 0, 0},
+                {1, 0, 0, 0, 0, 9, 5, 0, 0},
+                {8, 0, 0, 0, 0, 0, 0, 9, 0},
+                {0, 4, 3, 0, 0, 0, 7, 5, 0},
+                {0, 9, 0, 0, 0, 0, 0, 0, 8},
+                {0, 0, 9, 7, 0, 0, 0, 0, 5},
+                {0, 0, 0, 2, 0, 0, 0, 0, 0},
+                {0, 0, 7, 0, 4, 0, 2, 0, 3}
+        };
+        // Printing the board
+        printBoard(board);
+        System.out.println();
+        // Calling solveboard function
+        if (solveBoard(board)) {
+            System.out.println("Solved successfully!!!");
+        } else {
+            System.out.println("Unsolvable board :/");
+        }
+        System.out.println();
+        printBoard(board);
+    }
+
+    private static void printBoard(int[][] board) {
+        for(int row = 0; row < GRID_SIZE; row++) {
+            if(row % 3 == 0 && row != 0) {
+                System.out.println("---------------");
+            }
+            for(int column = 0; column < GRID_SIZE; column++) {
+                if(column % 3 == 0 && column != 0) {
+                    System.out.print(" | ");
+                }
+                System.out.print(board[row][column]);
+            }
+            System.out.println();
+        }
+    }
+
+    //If number already exists in the row
+    private static boolean isNumberInRow(int[][] board, int number, int row) {
+        for(int i = 0; i < GRID_SIZE; i++) {
+            if(board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //If number already exists in the column
+    private static boolean isNumberInColumn(int[][] board, int number, int column) {
+        for(int i = 0; i < GRID_SIZE; i++) {
+            if(board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //If number already exists in the box
+    private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+        int localBoxRow = row - row % 3;
+        int localBoxColumn = column - column % 3;
+
+        for(int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for(int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+                if(board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    // If the placement is valid
+    private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+        return !isNumberInRow(board, number, row) && !isNumberInColumn(board, number, column) &&
+                !isNumberInBox(board, number, row, column);
+    }
+    // For solving the board
+    private static boolean solveBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for(int column = 0; column < GRID_SIZE; column ++) {
+                if(board[row][column] == 0) {
+                    for(int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++) {
+                        if(isValidPlacement(board, numberToTry, row, column)) {
+                            board[row][column] = numberToTry;
+                            // Recursive call
+                            if (solveBoard(board)) {
+                                return true;
+                            } else {
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
